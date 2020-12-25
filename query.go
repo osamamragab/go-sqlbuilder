1e6bafb8ee9ac8e09c4e0c41109852b6ee46c17b
Osama <iosamaify@gmail.com>
2020-12-25 18:32:26 +0200

    query: add Insert

--- query.go
+++ query.go
@@ -1,12 +1,23 @@
 package sqlbuilder
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // Query describes an sql query.
 type Query struct {
 	str   *strings.Builder
 	args  []interface{}
 	table string
+}
+
+// NewQuery returns new Query with table.
+func NewQuery(table string) *Query {
+	return &Query{
+		str:   &strings.Builder{},
+		table: table,
+	}
 }
 
 // reset resets query string and arguments.
@@ -30,14 +41,6 @@ func (q *Query) Table() string {
 	return q.table
 }
 
-// NewQuery returns new Query with table.
-func NewQuery(table string) *Query {
-	return &Query{
-		str:   &strings.Builder{},
-		table: table,
-	}
-}
-
 func (q *Query) addColumns(columns ...string) {
 	for i, c := range columns {
 		q.str.WriteString(c)
@@ -45,6 +48,11 @@ func (q *Query) addColumns(columns ...string) {
 			q.str.WriteByte(',')
 		}
 	}
+}
+
+func (q *Query) addArg(arg interface{}) {
+	q.args = append(q.args, arg)
+	q.str.WriteString("$" + strconv.Itoa(len(q.args)))
 }
 
 // Select returns sql select statement.
@@ -57,5 +65,27 @@ func (q *Query) Select(columns ...string) *Statement {
 		q.str.WriteByte('*')
 	}
 	q.str.WriteString(" FROM " + q.table)
+	return &Statement{q}
+}
+
+// Insert returns sql insert statement.
+func (q *Query) Insert(columns []string, values ...[]string) *Statement {
+	q.reset()
+	q.str.WriteString("INSERT INTO " + q.table + " (")
+	q.addColumns(columns...)
+	q.str.WriteString(") VALUES ")
+	for i, vs := range values {
+		q.str.WriteByte('(')
+		for j, v := range vs {
+			q.addArg(v)
+			if j != len(vs)-1 {
+				q.str.WriteByte(',')
+			}
+		}
+		q.str.WriteByte(')')
+		if i != len(values)-1 {
+			q.str.WriteByte(',')
+		}
+	}
 	return &Statement{q}
 }
