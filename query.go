47bb5804dfbc3c9c7c57bf0da8e47ca0167de83f
Osama <iosamaify@gmail.com>
2020-12-25 17:42:22 +0200

    add Query methods

--- query.go
+++ query.go
@@ -1,14 +1,33 @@
 package sqlbuilder
 
-import (
-	"strings"
-)
+import "strings"
 
 // Query describes an SQL query.
 type Query struct {
 	str   *strings.Builder
 	args  []interface{}
 	table string
+}
+
+// reset resets query string and arguments.
+func (q *Query) reset() {
+	q.str.Reset()
+	q.args = nil
+}
+
+// String returns query string.
+func (q *Query) String() string {
+	return q.str.String()
+}
+
+// Args returns query arguments.
+func (q *Query) Args() []interface{} {
+	return q.args
+}
+
+// Table returns table name.
+func (q *Query) Table() string {
+	return q.table
 }
 
 // NewQuery returns new Query with table.
