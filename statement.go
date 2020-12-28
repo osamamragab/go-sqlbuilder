fe90038713dc61092568b730fd42e6466640e66e
Osama <iosamaify@gmail.com>
2020-12-28 22:16:01 +0200

    improve placeholder handling in Where

--- statement.go
+++ statement.go
@@ -1,6 +1,9 @@
 package sqlbuilder
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // Statement describes an sql query statement.
 type Statement struct {
@@ -8,32 +11,34 @@ type Statement struct {
 }
 
 // Where adds sql where condition to query.
-// Example:
-//	q.Select("name").Where("id = $@ AND username = $@", 1, "david")
-//	q.String() -> "SELECT name FROM tablename WHERE id = $1 AND username = $2"
-//	q.Args()   -> [1, "david"]
-//
-// "$@" are replaced with argument number.
 func (s *Statement) Where(str string, args ...interface{}) *Statement {
 	s.str.WriteString(" WHERE ")
-	if idx := strings.Index(str, "$@"); idx != -1 {
-		var i, last int
-		for idx != -1 && i < len(args) {
-			s.str.WriteString(str[last : last+idx])
-			s.addArg(args[i])
-			i++
-			last += idx + 2
-			idx = strings.Index(str[last:], "$@")
-		}
-		if len(str) > last {
-			s.str.WriteString(str[last:])
-		}
-	} else {
-		s.str.WriteString(str)
-		if args != nil {
-			s.args = append(s.args, args...)
+
+	if s.driver == "pg" {
+		idx := strings.IndexByte(str, '?')
+		if idx != -1 {
+			var i, last int
+			for idx != -1 && i < len(args) {
+				s.str.WriteString(str[last : last+idx])
+				s.args = append(s.args, args[i])
+				s.str.WriteString("$" + strconv.Itoa(len(s.args)))
+
+				i++
+				last += idx + 2
+				idx = strings.IndexByte(str[last:], '?')
+			}
+			if len(str) > last {
+				s.str.WriteString(str[last:])
+			}
+			return s
 		}
 	}
+
+	s.str.WriteString(str)
+	if args != nil {
+		s.args = append(s.args, args...)
+	}
+
 	return s
 }
 
